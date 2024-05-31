9c4d673479ab4533c9fe6668d8f3b101a92a323d
MelkinJoseMejia <70104262+MelkinJoseMejia@users.noreply.github.com>
2024-05-31 11:51:14 -0500

    Delete alianza-gml directory

--- alianza-gml/src/app/models/cliente.model.ts
+++
@@ -1,9 +0,0 @@
-export class Cliente {
-    id?: any;
-    nombre?: string;
-    telefono?: string;
-    email?: string;
-    sharedkey?: string;
-    fechainicio?: string;
-    fechafin?: string;
-}
